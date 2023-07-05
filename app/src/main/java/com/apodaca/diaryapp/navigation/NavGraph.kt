0d632465e395bf118453ab13cf731f5a12df7843
Apodaca <eduardo1ja99@gmail.com>
2023-07-04 18:34:59 -0600

    feat: auth screen

--- app/src/main/java/com/apodaca/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/apodaca/diaryapp/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.apodaca.diaryapp.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,6 +8,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.apodaca.diaryapp.presentation.screens.auth.AuthenticationScreen
 import com.apodaca.diaryapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 
 @Composable
@@ -23,9 +25,12 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
+        AuthenticationScreen(loadingState = false) {
 
+        }
     }
 }
 
