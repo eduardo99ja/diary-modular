f24e432a998d7113798a2226ab68c5c3e87279c1
Apodaca <eduardo1ja99@gmail.com>
2023-07-05 22:02:02 -0600

    feat: transparent status and navigation bardestination

--- app/src/main/java/com/apodaca/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/apodaca/diaryapp/navigation/NavGraph.kt
@@ -69,9 +69,7 @@ fun NavGraphBuilder.authenticationRoute(
                 viewModel.signInWithMongoAtlas(
                     tokenId = tokenId,
                     onSuccess = {
-                        if (it) {
-                            messageBarState.addSuccess("Successfully signed in")
-                        }
+                        messageBarState.addSuccess("Successfully signed in")
                         viewModel.setLoading(false)
                     },
                     onError = {
--- app/src/main/java/com/apodaca/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/apodaca/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
@@ -2,10 +2,15 @@ package com.apodaca.diaryapp.presentation.screens.auth
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import com.apodaca.diaryapp.util.Constants.CLIENT_ID
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
@@ -27,14 +32,19 @@ fun AuthenticationScreen(
     navigateToHome: () -> Unit,
     onButtonClicked: () -> Unit,
 ) {
-    Scaffold(content = {
-        ContentWithMessageBar(messageBarState = messageBarState) {
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
-        }
-    })
+    Scaffold(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        content = {
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
+        })
 
     OneTapSignInWithGoogle(
         state = oneTapState,
