983c248ae7c920c836481a7e555dd1a7c4ede80c
Apodaca <eduardo1ja99@gmail.com>
2023-07-09 20:44:26 -0600

    feat: create DateHeader for the sticky header

---
+++ app/src/main/java/com/apodaca/diaryapp/presentation/components/DiaryHolder.kt
@@ -0,0 +1,67 @@
+package com.apodaca.diaryapp.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+
+@Composable
+fun DateHeader(localDate: LocalDate) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Column(horizontalAlignment = Alignment.End) {
+            Text(
+                text = String.format("%02d", localDate.dayOfMonth),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = MaterialTheme.typography.titleLarge.fontWeight
+
+                )
+            )
+            Text(
+                text = localDate.dayOfWeek.toString().take(3),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = MaterialTheme.typography.bodySmall.fontWeight
+
+                )
+            )
+
+        }
+        Spacer(modifier = Modifier.width(14.dp))
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(
+                text = localDate.month.toString().lowercase().replaceFirstChar { it.uppercase() },
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = MaterialTheme.typography.titleLarge.fontWeight
+
+                )
+            )
+            Text(
+                text = "${localDate.year}",
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = MaterialTheme.typography.bodySmall.fontWeight
+
+                )
+            )
+
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DateHeaderPreview() {
+    DateHeader(LocalDate.now())
+}
